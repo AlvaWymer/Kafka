35a61c658f7a38c6d6e2507134e77c41bdfee325
yangnaihua <yangnaihua.2008@163.com>
2017-06-21 19:23:39 +0800

    Kafka 框架整合

---
+++ mykafka/src/main/java/cn/mldn/mykafka/producer/SendMessageProducer.java
@@ -0,0 +1,39 @@
+package cn.mldn.mykafka.producer;
+
+import java.util.Properties;
+
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.Producer;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.common.serialization.IntegerSerializer;
+import org.apache.kafka.common.serialization.StringSerializer;
+/**
+ * 生产者，没有验证功能
+ * @author paul
+ *
+ */
+public class SendMessageProducer {
+//	在本机的host中，指定ip
+	public static final String SERVERS = "kafka-single:9095";//映射的kafka 的主机的名字
+	public static final String TOPIC = "mldn-topic";
+	public static void main(String[] args) {
+		// 如果要想进行Kafka消息发送需要使用Properties定义一些环境属性
+		Properties props = new Properties();
+		props.setProperty(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, SERVERS); // 定义Kafka服务地址
+		// Kafka之中是以key和value的形式进行消息的发送处理， 所以为了保证Kafka的性能，专门提供有统一类型
+		// props.setProperty(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, "org.apache.kafka.common.serialization.StringSerializer") ;
+		props.setProperty(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName()) ;
+		props.setProperty(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, IntegerSerializer.class.getName()) ;
+		long start = System.currentTimeMillis() ;
+		// 定义消息发送者对象，依靠此对象可以进行消息的传递
+		Producer<String,Integer> producer = new KafkaProducer<String,Integer>(props) ;
+		for (int x = 0 ; x < 1 ; x ++) {
+			producer.send(new ProducerRecord<String,Integer>(TOPIC,"mldn-" + x,x)) ;
+		}
+		long end = System.currentTimeMillis() ;
+		System.out.println("*** 消息发送完成：" + (end - start));
+		producer.close(); 
+	}
+
+}
